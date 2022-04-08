9cd7576c128b2c98c96e6e624c223e55b1e4cb42
Ashraf-Raneem <ashrafraneem@gmail.com>
2022-04-08 19:25:29 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Footer from "./components/common/Footer/Footer";
+import Header from "./components/common/Header/Header";
+import Mint from "./components/common/Mint/Mint";
+import Team from "./pages/Team/Team";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Team/>
+      <Mint/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
